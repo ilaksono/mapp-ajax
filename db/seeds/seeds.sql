ad3ad733ebf6a320e906d0a67bc03f1a46e727ab
amarkham1 <andrewjmarkham1@gmail.com>
2020-10-16 19:18:47 +0000

    fix syntax error with db schema and seeds

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -4,10 +4,10 @@ INSERT INTO users (username, email, password) VALUES ('testuser3', 'test3@gmail.
 INSERT INTO users (username, email, password) VALUES ('testuser4', 'test4@gmail.com', 'password');
 
 INSERT INTO markers (map_id, latitude, longitude, title, description, image_url)
-VALUES (1, 38.8951, -770364, 'Washington, DC',"Shopper's Drug Mart",'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
-(2, 48.8951, -770364, 'Some Place',"McDonald's",'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
-(3, 58.8951, -770364, 'Somewhere',"Applebee's",'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
-(4, 68.8951, -770364, 'Some place',"Golf Course",'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350');
+VALUES (1, 38.8951, -770364, 'Washington, DC', 'Shopper''s Drug Mart', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
+(2, 48.8951, -770364, 'Some Place', 'McDonald''s', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
+(3, 58.8951, -770364, 'Somewhere', 'Applebee''s', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350'),
+(4, 68.8951, -770364, 'Some place', 'Golf Course', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h350');
 
 INSERT INTO maps (title, description) VALUES ('Cool Map', 'Map showing cool locations');
 INSERT INTO maps (title, description) VALUES ('Food Map', 'Map showing great food');
