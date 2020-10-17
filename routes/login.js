e3656447e4035011090a71edb77832b8131df960
David Folkins <david.folkins88@gmail.com>
2020-10-17 11:49:37 -0400

    Add user error handling if email or password is blank

--- routes/login.js
+++ routes/login.js
@@ -13,9 +13,22 @@ module.exports = (db) => {
     WHERE email = $1
     AND password = $2
     `
-    res.redirect('maps')
-    return db.query(query, [user.email, user.password])
+    db.query(query, [user.email, user.password])
+    .then(response => {
+      console.log(response.rows[0].username)
+      constDbUser = response.rows[0]
+      if (constDbUser.email.length > 0) {
+        console.log("hello", constDbUser.username)
+        res.redirect('login')
+      }
+      res.send("Please enter email and password")
+    })
+
   });
 
   return router;
 };
+
+// if (user.email === "" || user.password === "") {
+//   res.send("403: Failed to login");
+// }
