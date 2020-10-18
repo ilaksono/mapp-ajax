a77ebb388b36922abb3dd2e3504ae051639a0820
David Folkins <david.folkins88@gmail.com>
2020-10-18 16:22:30 -0400

    Add 2 helper functions that get contributors and favorites by id

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,10 @@ module.exports = (db) => {
   router.get("/:id", (req, res) => {
     console.log(req.params.id);
     dbHelpers.getUserById(req.params.id)
+    // dbHelpers.getContributorById(req.params.id)
+    // dbHelpers.getFavouritesById(req.params.id)
     .then(data => {
+      console.log(data)
       const users = data.username;
       const title = data.title
       const description = data.description
