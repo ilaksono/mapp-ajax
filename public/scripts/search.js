2047c3400272c46652eda36ebd50adf3d77e07f0
amarkham1 <andrewjmarkham1@gmail.com>
2020-10-22 12:47:09 +0000

    add search feature

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,4 +1,19 @@
 
 $(document).ready(() => {
+  $('#search').on('input', function(event) {
+    loadCardsMatchingSearch(event.target.value);
+  });
 
-}
+  const loadCardsMatchingSearch = string => {
+    const $container = $('.section-container');
+    for (const child of $($container).children()) {
+      const title = child.children[1].children[0].innerText.toLowerCase();
+      const description = child.children[1].children[1].innerText.toLowerCase();
+      if (title.includes(string.toLowerCase()) || description.includes(string.toLowerCase())) {
+        $(child).first().attr("hidden", false);
+      } else {
+        $(child).first().attr("hidden", true);
+      }
+    }
+  }
+});
